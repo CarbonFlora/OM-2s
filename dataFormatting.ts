d9506b6503a68b3341bd6038c369d072878e3edc
CarbonFlora <zihaoliang0413@gmail.com>
2022-08-23 10:35:25 -0700

    learning lowdb

--- dataFormatting.ts
+++ dataFormatting.ts
@@ -3,5 +3,4 @@ const fs = require('fs');
 
 
 
-//fs.writeFileSync('./responses.json',JSON.stringify(cleanResponses, null, 2))
 //fs.writeFileSync('./cleanArrayData.json',JSON.stringify(arrayDataInput, null, 2))
